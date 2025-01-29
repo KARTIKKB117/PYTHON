cf94d5617805a890d919f8ad5f52ffc5e526b603
KARTIKKB117 <bandekar.kartik117@gmail.com>
2025-01-29 10:58:15 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,5 @@
+#Slicing of strings
+
+name = "kunal"
+nameshort = name[0:3]
+### taking vlue from 0 till 3 excluding 3
---
+++ typeconversionncasting.py
@@ -0,0 +1,11 @@
+# it tells the class 
+
+a=31
+t=type(a) #<class'int'>
+print(t)
+
+c = "33.3"# inside the double cotes it is string
+b= float(c)#string to float
+t= type(b)
+
+print(t)
