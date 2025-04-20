0060ca198f995b80de3c602f94e681daf9917699
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2025-04-20 19:35:41 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class employee :
+    language = 'py'
+    salary = 1000
+    
+harry = employee()
+harry.name = 'harry'
+print(harry.name ,harry.language, harry.salary)
+    
+rohan = employee()
+rohan.name = 'rohan'
+print(rohan.name ,rohan.language, rohan.salary)
